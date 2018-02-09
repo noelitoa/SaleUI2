06bc37dabddfb8e3ff630efbcc0ec3eb6aa767dc
A\narlante <narlante@mssngsolutions.ph>
2018-02-09 17:20:09 +0800

    Added delete, update and search pending to product

--- SaleUI2/Pages/ProductIndex.cshtml.cs
+++ SaleUI2/Pages/ProductIndex.cshtml.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
 using System.Net.Http;
+using System.Text;
 using Newtonsoft.Json;
 using SaleUI2.Models;
 using X.PagedList;
@@ -57,6 +59,80 @@ namespace SaleUI2.Pages
             
         }
 
+        public async Task<IActionResult> OnPostUpdateAsync(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+            product.TimeStamp = DateTime.Now;
+
+            var stringContent = new StringContent(JsonConvert.SerializeObject(product), Encoding.UTF8,
+                "application/json");
+            var response = await client.PutAsync(uri + "product/" + product.Id, stringContent);
+
+            if (response.IsSuccessStatusCode)
+            {
+                var location = response.Headers.GetValues("location").FirstOrDefault();
+                if (location != null)
+                {
+                    var _itemId = location.Substring(location.LastIndexOf('/') + 1);
+                    return RedirectToPage("/ProductIndex", new { id = _itemId });
+                }
+
+                return RedirectToPage("/Error");
+            }
+
+            return RedirectToPage("/Error");
+        }
+
+        public void OnPostSearchAsync(string query)
+        {
+            var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+
+            Products = GetAsJsonSync<List<Product>>(uri + "Product/search/"+ query + "/_id/0");
+            PageProducts = Products.ToPagedList(1, DefaultPageSize);
+            
+        }
+
+        // http://localhost:1636/api/Product/search/asdf/_id/0
+
+        public async Task<IActionResult> OnGetDelete(string id)
+        {
+            if (!String.IsNullOrEmpty(id))
+            {
+                var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+                var response = await DeleteAsString(uri + "product/" + id);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var itemId = id;
+                    return RedirectToPage("/ProductIndex");
+                }
+            }
+
+            return RedirectToPage("/Error");
+        }
+
+        public async Task<IActionResult> OnPostDelete(string id)
+        {
+            if (!String.IsNullOrEmpty(id))
+            {
+                var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+                var response = await DeleteAsString(uri + "product/" + id);
+
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    var itemId = id;
+                    return RedirectToPage("/ProductIndex");
+                }
+            }
+
+            return RedirectToPage("/Error");
+        }
+
 
         #region Private
 
@@ -96,6 +172,13 @@ namespace SaleUI2.Pages
             }
 
             return default(T);
+        }
+
+        private async Task<HttpResponseMessage> DeleteAsString(string requestUri)
+        {
+            var jsonResponse = await client.DeleteAsync(requestUri);
+
+            return jsonResponse;
         }
 
         #endregion
