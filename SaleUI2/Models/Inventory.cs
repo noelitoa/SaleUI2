bd1c1e2d0e4df410e22ff42b005a2335af34c039
A\narlante <narlante@mssngsolutions.ph>
2018-02-15 15:20:50 +0800

    Added inventory index

---
+++ SaleUI2/Models/Inventory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaleUI2.Models
+{
+    public class Inventory
+    {
+        public string Id { get; set; }
+        [Required]
+        public string ProductSKU { get; set; }
+        [Required]
+        public string ProductName { get; set; }
+        public string LazadaSKU { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public DateTime LastSaleDate { get; set; }
+        public DateTime TimeStamp { get; set; }
+        public string EncodedBy { get; set; }
+    }
+}
---
+++ SaleUI2/Pages/InventoryIndex.cshtml.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using SaleUI2.Models;
+using X.PagedList;
+
+namespace SaleUI2.Pages
+{
+    public class InventoryIndexModel : PageModel
+    {
+
+        private static HttpClient client;
+        private IConfiguration _configuration;
+
+        private const int DefaultPageSize = 20;
+
+        [BindProperty]
+        public Inventory Inventory { get; set; }
+        [BindProperty]
+        public string ConfirmMessage { get; set; }
+
+        public List<Inventory> Inventories { get; set; }
+        public IPagedList<Inventory> PageInventories { get; set; }
+
+
+
+        public InventoryIndexModel(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            client = GetHttpClient();
+        }
+
+
+        public void OnGet(string id, string confirm)
+        {
+            if (!String.IsNullOrEmpty(id))
+            {
+                var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+                var inventories = GetAsJsonSync<List<Inventory>>(uri + "Inventory/inventory/" + id);
+                Inventory = inventories.FirstOrDefault();
+
+                if (!String.IsNullOrEmpty(confirm) && confirm == "ok")
+                {
+                    ConfirmMessage = $"Update successful to id = {id}.";
+                }
+            }
+            else
+            {
+                var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+                Inventories = GetAsJsonSync<List<Inventory>>(uri + "Inventory/all/0/999/productSKU.keyword/0");
+                PageInventories = Inventories.ToPagedList(1, DefaultPageSize);
+            }
+        }
+
+        public void OnGetPaging(int id)
+        {
+            var uri = _configuration.GetSection("SaleEsApi").GetSection("Uri").Value;
+            Inventories = GetAsJsonSync<List<Inventory>>(uri + "Inventory/all/0/999/productSKU.keyword/0");
+            PageInventories = Inventories.ToPagedList(id, DefaultPageSize);
+
+        }
+
+        #region Private
+
+        public HttpClient GetHttpClient()
+        {
+            if (client == null)
+            {
+                return new HttpClient();
+            }
+
+            return client;
+        }
+
+        private async Task<T> GetAsJson<T>(string requestUri)
+        {
+            var jsonResponse = await client.GetAsync(requestUri);
+
+            using (var content = jsonResponse.Content)
+            {
+                var json = content.ReadAsStringAsync();
+                return JsonConvert.DeserializeObject<T>(json.Result);
+            }
+        }
+
+        private T GetAsJsonSync<T>(string requestUri)
+        {
+            var response = client.GetAsync(requestUri).Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                var responseContent = response.Content;
+
+                // by calling .Result you are synchronously reading the result
+                var responseString = responseContent.ReadAsStringAsync().Result;
+
+                return JsonConvert.DeserializeObject<T>(responseString);
+            }
+
+            return default(T);
+        }
+
+        private async Task<HttpResponseMessage> DeleteAsString(string requestUri)
+        {
+            var jsonResponse = await client.DeleteAsync(requestUri);
+
+            return jsonResponse;
+        }
+        #endregion
+    }
+}
